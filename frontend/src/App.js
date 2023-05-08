3f5054abbfc30ee349786979fc3cff2b346c60d1
Arian Jahiri <ajahiri.AIT@gmail.com>
2023-05-08 19:00:49 +1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
     setCurrentPage(page);
     window.scrollTo(0, 0);
     fetch(
-      `https://backend.wiki-search.arianjahiri.com/search?query=${query}&limit=${limit}&offset=${
+      `https://wikisearchbackend.arianjahiri.com/search?query=${query}&limit=${limit}&offset=${
         (page || 0) * limit
       }&use_llm=${useLLM}`,
       { method: "GET" }
@@ -54,7 +54,7 @@ function App() {
     setLoadingContent(true);
     setCurrentPage(0);
     fetch(
-      `https://backend.wiki-search.arianjahiri.com/search?query=${query}&limit=${limit}&offset=${0}&use_llm=${useLLM}`,
+      `https://wikisearchbackend.arianjahiri.com/search?query=${query}&limit=${limit}&offset=${0}&use_llm=${useLLM}`,
       { method: "GET" }
     )
       .then((response) => response.json())
